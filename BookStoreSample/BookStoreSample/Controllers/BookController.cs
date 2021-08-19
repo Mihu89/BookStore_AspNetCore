4ea31814d07bfcc24b5d1cfb8e1810eda27dada0
MihaiCoscodan <mihail.coscodan@bergenlogistics.com>
2021-08-19 19:34:38 +0300

    Add Book Controller

---
+++ BookStoreSample/BookStoreSample/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using BookStoreSample.Models;
+using BookStoreSample.Repos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreSample.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepo _bookRepo;
+
+        public BookController(BookRepo bookRepo)
+        {
+            _bookRepo = bookRepo;
+        }
+
+        public IActionResult GetAllBooks()
+        {
+            var books = _bookRepo.GetAll();
+            return View(books);
+        }
+
+        public Book GetBook(int id)
+        {
+            return _bookRepo.GetById(id);
+        }
+
+        public Book Edit(int id)
+        {
+            return _bookRepo.GetById(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public Book Edit(Book model, int id)
+        {
+            return _bookRepo.Edit(model, id);
+        }
+
+
+        public List<Book> SearchBooks(string name, string author)
+        {
+            return _bookRepo.SearchBooks(name, author);
+        }
+    }
+}
---
+++ BookStoreSample/BookStoreSample/Repos/BookRepo.cs
@@ -0,0 +1,49 @@
+﻿using BookStoreSample.Data;
+using BookStoreSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreSample.Repos
+{
+    public class BookRepo
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BookRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Book> GetAll()
+        {
+            return _dbContext.Books.ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _dbContext.Books.FirstOrDefault(x => x.Id == id); // && x.IsDeleted == false
+        }
+
+        public List<Book> SearchBooks(string title, string author)
+        {
+            return _dbContext.Books.Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+
+        public Book Edit(Book book, int id)
+        {
+            var existingBook = _dbContext.Books.FirstOrDefault(x => x.Id == id); // && x.IsDeleted == false
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                existingBook.PuplishYear = book.PuplishYear;
+                existingBook.IsDeleted = book.IsDeleted;
+                _dbContext.SaveChanges();
+
+            }
+            return existingBook;
+        }
+
+    }
+}
